f90b663b419c48975f01bc3d52f94403b7401649
Sam <samrg123@gmail.com>
2022-07-08 15:29:10 -0400

    Added Get ExceptionHandler

--- util.h
+++ util.h
@@ -15,6 +15,7 @@
 // TODO: Move this out to stringUtil.h
 #include <WString.h>
 
+// TODO: Move to intellisense.h
 // Hack to prevent intelisense failing to infer templates in WString.h
 #ifdef __INTELLISENSE__
     template<typename T>
@@ -43,3 +44,19 @@ auto CriticalSection(LambdaT lambda) {
     }
 } 
 
+// TODO: Pull this out to a better location
+// TODO: Define EXCCAUSE constants ... right now they're only accessible in xtensa/corebits after you include GDBStub.h,
+//       But we may want to access them when debugging is disabled.
+using ExceptionHandler = fn_c_exception_handler_t;
+
+inline ExceptionHandler GetExceptionHandler(int exceptionCause) {
+
+    return CriticalSection([=]() {
+        
+        //temporarily set exception handler to default, capture old handler and restore it  
+        ExceptionHandler handler = _xtos_set_exception_handler(exceptionCause, nullptr);
+        _xtos_set_exception_handler(exceptionCause, handler);
+
+        return handler;
+    });
+}
